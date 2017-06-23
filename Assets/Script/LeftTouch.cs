cc96a79005e659f89c552cbc3d4e32f4e3664f25
lalalaWeiJian <765579323@qq.com>
2017-06-23 15:58:55 +0800

    left joyStick/move

---
+++ Assets/Script/LeftTouch.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftTouch : MonoBehaviour {
+
+    public RectTransform leftTouchButton;
+    public RectTransform bgPanel;
+   // public RectTransform 
+
+	// Use this for initialization
+	void Start () {
+        leftTouchButton = GameObject.Find("LeftTouchButton").GetComponent<RectTransform>();
+        bgPanel = GameObject.Find("bg").GetComponent<RectTransform>();
+	
+	}
+
+    //private void OnMouseDrag()
+    //{
+    //    Debug.Log(bgPanel.position.x + ";" + bgPanel.position.y);
+    //}
+    //private void OnMouseOver()
+    //{
+    //    Debug.Log(bgPanel.position.x + ";" + bgPanel.position.y);
+    //}
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Debug.Log(mousePosition.x + ";" + mousePosition.y);
+    }
+
+    public void onPress()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+        
+
+
+
+    }
+}
---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public JoyStick leftJoyStick;           //左手柄的控制脚本
+    //public JoyStick rightJoyStick;        //右手柄的控制脚本
+
+    public float moveStep = 7f;                   
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+
+
+	void Start ()
+    {
+        leftJoyStick.OnJoyStickTouchBegin += OnJoyStickBegin;
+        leftJoyStick.OnJoyStickTouchMove += OnJoyStickMove;
+        leftJoyStick.OnJoyStickTouchEnd += OnJoyStickEnd;
+
+        controller = GetComponent<CharacterController>();
+    }
+		
+	void Update ()
+    {        
+
+    }
+
+    void OnJoyStickBegin(Vector2 move)
+    {
+        Debug.Log("touch begin.");
+    }
+
+    void OnJoyStickMove(Vector2 move)
+    {
+        Vector3 dir = new Vector3(move.x * moveStep * Time.deltaTime, 0, move.y * moveStep * Time.deltaTime);
+        controller.Move(dir);
+    }
+
+    void OnJoyStickEnd()
+    {
+        Debug.Log("touch end.");
+    }
+}
