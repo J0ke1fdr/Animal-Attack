d96ff6863b97d4c42b2c61d35685c20ea74627c6
J0ke1fdr <506860078@qq.com>
2017-06-23 16:01:25 +0800

    Simple AI test

---
+++ Assets/Script/findEnemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class findEnemy : MonoBehaviour {
+
+    private GameObject currentEnemy = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(currentEnemy != null)
+            transform.LookAt(currentEnemy.transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentEnemy = other.gameObject;
+        Debug.Log("find enemy");
+        
+    }
+}
