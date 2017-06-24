c9c8a9b73020a509451ce03fb85f14273ecb1ed4
lalalaWeiJian <765579323@qq.com>
2017-06-24 09:03:43 +0800

    右手摇杆/人物转向

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
     public JoyStick leftJoyStick;           //左手柄的控制脚本
-    //public JoyStick rightJoyStick;        //右手柄的控制脚本
+    public JoyStick rightJoyStick;        //右手柄的控制脚本
 
     public float speed = 0.07f;                   
 
     private CharacterController controller;
     private Vector3 moveDirection;
+    private Vector3 flipDirection;
     private bool moving = false;
 
 	void Start ()
     {
-        leftJoyStick.OnJoyStickTouchBegin += OnJoyStickBegin;
-        leftJoyStick.OnJoyStickTouchMove += OnJoyStickMove;
-        leftJoyStick.OnJoyStickTouchEnd += OnJoyStickEnd;
+        leftJoyStick.OnJoyStickTouchBegin += OnLeftJoyStickBegin;
+        leftJoyStick.OnJoyStickTouchMove += OnLeftJoyStickMove;
+        leftJoyStick.OnJoyStickTouchEnd += OnLeftJoyStickEnd;
+
+        rightJoyStick.OnJoyStickTouchBegin += OnRightJoyStickBegin;
+        rightJoyStick.OnJoyStickTouchMove += OnRightJoyStickMove;
+        rightJoyStick.OnJoyStickTouchEnd += OnRightJoyStickEnd;
 
         controller = GetComponent<CharacterController>();
     }
@@ -28,24 +33,41 @@ public class PlayerControl : MonoBehaviour {
             controller.Move(moveDirection * speed);
         }
     }
-
-    void OnJoyStickBegin(Vector2 move)
+    //左手摇杆接触开始
+    void OnLeftJoyStickBegin(Vector2 move)
     {
-        Debug.Log("touch begin.");
+        //Debug.Log("touch begin.");
         moving = true;
     }
-
-    void OnJoyStickMove(Vector2 move)
+    //左手摇杆移动
+    void OnLeftJoyStickMove(Vector2 move)
     {
-        Debug.Log("move: " + move.x + "," + move.y);
-        //Vector3 dir = new Vector3(move.x * speed, 0, move.y * speed);
-        //controller.Move(dir);
+        //Debug.Log("move: " + move.x + "," + move.y);
         moveDirection = new Vector3(move.x, 0, move.y);
     }
-
-    void OnJoyStickEnd()
+    //左手摇杆接触结束
+    void OnLeftJoyStickEnd()
     {
-        Debug.Log("touch end.");
+        //Debug.Log("touch end.");
         moving = false;
     }
+
+    //右手摇杆接触开始
+    void OnRightJoyStickBegin(Vector2 move)
+    {
+        //开火
+    }
+    //右手摇杆移动
+    void OnRightJoyStickMove(Vector2 move)
+    {
+        flipDirection = new Vector3(move.x, 0, move.y);
+
+        transform.rotation = Quaternion.LookRotation(flipDirection );
+    }
+    //右手摇杆接触结束
+    void OnRightJoyStickEnd()
+    {
+
+    }
+
 }
