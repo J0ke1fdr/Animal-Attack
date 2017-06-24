0170da76c9cddd349af456312fbb5b9446644e1e
Reaky <reakys0429@live.com>
2017-06-24 17:07:56 +0800

    增加部分菜单及功能
    1.增加主菜单按钮
    2.增加设置窗口

---
+++ Assets/UI_Floder/UI_Script/MainMenu/Setting/ToggleSwitch.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ToggleSwitch : MonoBehaviour {
+
+    public Toggle Toggle;
+   
+    private Slider bgmSlider;
+    private Slider soundEffectSlider;
+
+    private Text musicToggleText;
+    private Text soundEffectToggleText;
+	// Use this for initialization
+	void Start () {
+        Toggle = gameObject.GetComponent<Toggle>();
+        if (gameObject.name == "bgmToggle")
+        {
+            bgmSlider = GameObject.Find("music/Slider").GetComponent<Slider>();
+            musicToggleText = GameObject.Find("bgmToggle/Label").GetComponent<Text>();
+        }
+        else if(gameObject.name == "soundEffectToggle")
+        {          
+            soundEffectSlider = GameObject.Find("soundEffect/Slider").GetComponent<Slider>();
+            soundEffectToggleText = GameObject.Find("soundEffectToggle/Label").GetComponent<Text>();
+        }
+	}
+	
+    public void onToggleSwitch()
+    {
+        if(Toggle.isOn)
+        {
+            Debug.Log(gameObject.name);
+            if(Toggle.gameObject.name == "bgmToggle")
+            {
+                bgmSlider.enabled = true;
+                musicToggleText.text = "开";
+            }
+            else if(gameObject.name == "soundEffectToggle")
+            {
+                soundEffectSlider.enabled = true;
+                soundEffectToggleText.text = "开";
+            }
+            
+        }
+        else
+        {
+            Debug.Log(gameObject.name);
+            if (Toggle.gameObject.name == "bgmToggle")
+            {
+                bgmSlider.enabled = false;
+                musicToggleText.text = "关";
+
+            }
+            else if (Toggle.gameObject.name == "soundEffectToggle")
+            {
+                soundEffectSlider.enabled = false;
+                soundEffectToggleText.text = "关";
+            }
+        }
+    }
+}
---
+++ Assets/UI_Floder/UI_Script/MainMenu/Setting/closeSettingPanel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class closeSettingPanel : MonoBehaviour {
+
+    private Image SettingPanel;
+	// Use this for initialization
+	void Start () {
+        SettingPanel = GameObject.Find("SettingPanel").GetComponent<Image>();
+	}
+	
+	public void CloseSettingPanel()
+    {
+        SettingPanel.gameObject.SetActive(false);
+    }
+}
