b926aeac2d74cd95b418ff1780a69d107340a36b
Reaky <reakys0429@live.com>
2017-06-24 10:05:44 +0800

    修复问题
    1.修复摇杆在不同屏幕比例下变形的问题
    2.增加武器切换按钮

---
+++ Assets/UI_Floder/UI_Script/Wepon/weponChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weponChange : MonoBehaviour {
+
+    public void onWeaponChangeClick()
+    {
+       // Debug.Log("点击武器切换按钮");
+    }
+}
