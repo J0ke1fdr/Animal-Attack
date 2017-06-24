7caae4b90718de143a79d6fc9a210aa4294dd82c
XvWenJun <906626481@qq.com>
2017-06-24 10:23:14 +0800

    添加气罐物体交互脚本

---
+++ Assets/Script/SceneScript/Barrel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour
+{
+    public float health = 100f;
+
+    public GameObject specialEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        health -= 10;
+        if (health <= 0f)
+            UpdateStatus();
+    }
+
+    private void UpdateStatus()
+    {
+        GameObject effect = Instantiate(specialEffect, transform.position, Quaternion.identity) as GameObject;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/SceneScript/TrashCanEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashCanEffect : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if(other.gameObject.tag=="")
+        //减速
+    }
+}
