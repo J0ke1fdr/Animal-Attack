5abe01a6f16341af609661143e226d4c769e3d6d
XvWenJun <906626481@qq.com>
2017-06-28 00:49:09 +0800

    夜景尺寸和烘培更改

--- Assets/Script/JoyStick.cs
+++ Assets/Script/JoyStick.cs
@@ -9,26 +9,32 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
     /// 以像素为单位
     /// </summary>
     public float JoyStickRadius = 20;
+
     /// <summary>
     /// 摇杆重置所诉
     /// </summary>
     public float JoyStickResetSpeed = 5.0f;
+
     /// <summary>
     /// 当前物体的Transform组件
     /// </summary>
     private RectTransform selfTransform;
+
     /// <summary>
     /// 是否触摸了虚拟摇杆
     /// </summary>
     public bool isTouched = false;//获取
+
     /// <summary>
     /// 虚拟摇杆的默认位置
     /// </summary>
     private Vector2 originPosition;
+
     /// <summary>
     /// 虚拟摇杆的移动方向
     /// </summary>
     private Vector2 touchedAxis;
+
     public Vector2 TouchedAxis
     {
         get
@@ -38,38 +44,45 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
             return touchedAxis.normalized;
         }
     }
+
     /// <summary>
     /// 定义触摸开始事件委托
     /// </summary>
     public delegate void JoyStickTouchBegin(Vector2 vec);
+
     /// <summary>
     /// 定义触摸过程事件委托
     /// </summary>
     /// <param name="vec">虚拟摇杆的移动方向</param>
     public delegate void JoyStickTouchMove(Vector2 vec);
+
     /// <summary>
     /// 定义触摸结束事件委托
     /// </summary>
     public delegate void JoyStickTouchEnd();
+
     /// <summary>
     /// 注册触摸开始事件
     /// </summary>
     public event JoyStickTouchBegin OnJoyStickTouchBegin;
+
     /// <summary>
     /// 注册触摸过程事件
     /// </summary>
     public event JoyStickTouchMove OnJoyStickTouchMove;
+
     /// <summary>
     /// 注册触摸结束事件
     /// </summary>
     public event JoyStickTouchEnd OnJoyStickTouchEnd;
 
-    void Start()
+    private void Start()
     {
         //初始化虚拟摇杆的默认方向
         selfTransform = this.GetComponent<RectTransform>();
         originPosition = selfTransform.anchoredPosition;
     }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         isTouched = true;
@@ -77,6 +90,7 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
         if (this.OnJoyStickTouchBegin != null)
             this.OnJoyStickTouchBegin(TouchedAxis);
     }
+
     public void OnPointerUp(PointerEventData eventData)
     {
         isTouched = false;
@@ -85,13 +99,15 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
         if (this.OnJoyStickTouchEnd != null)
             this.OnJoyStickTouchEnd();
     }
+
     public void OnDrag(PointerEventData eventData)
     {
         touchedAxis = GetJoyStickAxis(eventData);
         if (this.OnJoyStickTouchMove != null)
             this.OnJoyStickTouchMove(TouchedAxis);
     }
-    void Update()
+
+    private void Update()
     {
         //当虚拟摇杆移动到最大半径时摇杆无法拖动
         //为了确保被控制物体可以继续移动
@@ -102,14 +118,13 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
                 this.OnJoyStickTouchMove(TouchedAxis);
         }
         //松开虚拟摇杆后让虚拟摇杆回到默认位置
-        if ( !isTouched && selfTransform.anchoredPosition.magnitude > originPosition.magnitude)
+        if (!isTouched && selfTransform.anchoredPosition.magnitude > originPosition.magnitude)
             selfTransform.anchoredPosition -= TouchedAxis * Time.deltaTime * JoyStickResetSpeed;
 
-
-      //  Debug.Log("x:"+TouchedAxis.x + ";y:" + TouchedAxis.y);
-       // Debug.Log("当前偏移角度：" +"PI/" + Mathf.Floor(Mathf.PI/Mathf.Atan2(TouchedAxis.y, TouchedAxis.x)));
-
+        //  Debug.Log("x:"+TouchedAxis.x + ";y:" + TouchedAxis.y);
+        // Debug.Log("当前偏移角度：" +"PI/" + Mathf.Floor(Mathf.PI/Mathf.Atan2(TouchedAxis.y, TouchedAxis.x)));
     }
+
     /// <summary>
     /// 返回虚拟摇杆的偏移量
     /// </summary>
@@ -132,5 +147,4 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
         }
         return touchAxis;
     }
-
 }
