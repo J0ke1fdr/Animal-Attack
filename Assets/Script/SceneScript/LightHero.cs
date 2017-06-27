52a1cbce98c8c3cbb4132f9e9e1fae1475903fd9
XvWenJun <906626481@qq.com>
2017-06-27 17:18:30 +0800

    多人联机场景特效添加，摄像机边界位置更改

---
+++ Assets/Script/SceneScript/LightHero.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightHero : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.LookAt(player);
+    }
+}
