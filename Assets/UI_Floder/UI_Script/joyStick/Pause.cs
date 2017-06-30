4eab82b8f21e8b3c887d9d4c636e89b2993117f4
Reaky <reakys0429@live.com>
2017-06-30 11:35:51 +0800

    增加FPS显示和暂停功能
    发布至Reaky01

---
+++ Assets/UI_Floder/UI_Script/joyStick/Pause.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+
+	public void onPauseClick()
+    {
+        if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else if(Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
